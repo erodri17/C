4d93726bb1b35c121d3815ac444759d6d2b7f654
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2016-10-23 22:53:31 -0400

    "program1.c" has been added to the Programs folder
    “program1.c” that is a program that prints ”To C or not to C: that is
    the question.” every time is run has been added to the Programs folder.

---
+++ Programs/program1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Name: program1.c
+ * Purpose: Prints a statement
+ * @return  [description]
+ */
+
+int main(void)
+{
+printf("To C or not to C: that is the question.\n");
+return 0;
+}
+
+
+/**
+ * Works Cited
+ *
+ * King, K. N. C Programming: A Modern Approach. New York: W.W. Norton, 2008. Print.
+ *
+ * */
