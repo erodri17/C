06040683b6050529cdc8879bf390e7926808bdb4
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2016-10-23 23:12:33 -0400

    "program5.c" has been added to the Programs folder
    “program5.c” is a simple program that adds (only) two fractions and
    prints out the result. The fractions that are going to be added need to
    be given by the user.

---
+++ Programs/program5.c
@@ -0,0 +1,28 @@
+/**
+ * Name: program5.c
+ * Purpose: Adds two fractions
+ * @return  [number]
+ */
+#include<stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, result_denom;
+
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+
+    result_num = num1 * denom2 + num2 * denom1;
+    result_denom = denom1 * denom2;
+    printf("The sum is %d/%d\n", result_num, result_denom);
+    return 0;
+}
+
+/**
+ * Works Cited
+ *
+ * King, K. N. C Programming: A Modern Approach. New York: W.W. Norton, 2008. Print.
+ *
+ * */
