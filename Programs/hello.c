c45cdb3b6007723c655d153fdbb8e0454371beb4
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2016-10-23 22:49:02 -0400

    Folder "Programs" has been added.
    “Programs” contains hello.c program.

---
+++ Programs/hello.c
@@ -0,0 +1,18 @@
+/**
+ * Name: program5.c
+ * Purpose: Hello World Program
+ *
+ */
+#include<stdio.h>
+
+int main(void)
+{
+    printf("hello, world!\n");
+
+}
+
+/**
+ * Works Cited
+ *
+ * King, K. N. C Programming: A Modern Approach. New York: W.W. Norton, 2008. Print.*/
+ */
