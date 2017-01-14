04203bfd03544ec4c18b7af54ac833a666244a31
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2017-01-13 21:27:56 -0500

    fahr.c
    Proper documentation has been added to the code.

--- Programs/fahr.c
+++ Programs/fahr.c
@@ -1,24 +1,33 @@
-/*This program uses the formula C = (5/9)(F-32) to print the following table of Fahrenheit temperatures and their centigrade or Celsius equivalents*/
+/*
+    fahr.c
+    Purpose: This program prints a Fahrenheit-Celsius table for fahr = 0,20, 40 .. , 300
+    @author Elmer Rodriguez
+    @version 1.0 01/13/2017
 
+ */
 
+//This program uses the formula C = (5/9)(F-32) to print the following table of Fahrenheit temperatures and their centigrade or Celsius equivalents
+
+//importing the stdio.h function from the C language library
 #include<stdio.h>
-/*print Fahrenheit-Celsius table for fahr = 0,20, 40 .. , 300*/
 
 main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    int fahr, celsius; //declaring the variables representing the temperature units
+    int lower, upper, step;// declaring the variables that reprensent the upper level, lower level and step (range between temperatures)
 
-    lower = 0; /*lower limit of temperature table*/
-    upper = 300; /*upper limit*/
-    step = 20; /*step size*/
+    //initializing the values (providing them values)
+    lower = 0; //lower limit of temperature table
+    upper = 300; //upper limit
+    step = 20; //step size
 
-    fahr = lower;
-    while (fahr <= upper){
-        celsius = 5*(fahr-32)/9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr+step;
+    fahr = lower;//setting the temperature in Fahrenheit to the lower limit (which is zero in the case)
 
+    while (fahr <= upper)//while the temperature in fahrenheit is less than the upper limit, keep printing the table
+    {
+        celsius = 5*(fahr-32)/9; // Fahranheit to Celcius formula is stored in the "celsius" variable
+        printf("%d\t%d\n", fahr, celsius);//Printing the result (in table form)
+        fahr = fahr+step;//adding the step (range between elements) to the fahrenheit temperature
     }
 
 }
